eda4726a50d13d870587ebe5a9f116efa5bd639b
Moisés López <moylop260@vauxoo.com>
2016-02-21 00:18:22 -0600

    squash

--- logging_test.py
+++ logging_test.py
@@ -186,6 +186,7 @@ PSEUDOCONFIG_MAPPER = {
     'critical': ['openerp:CRITICAL', 'werkzeug:CRITICAL'],
 }
 
+
 if __name__ == '__main__':
     init_logger()
     import logging
@@ -193,92 +194,3 @@ if __name__ == '__main__':
     _logger.info('hola mundo 1')
     _logger.warn('hola mundo 2')
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
-'''
-#!/usr/bin/env python
-
-import argparse
-import os
-import subprocess
-import logging
-
-
-BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
-
-# The background is set with 40 plus the number of the color, and the
-# foreground with 30
-
-# These are the sequences need to get colored ouput
-RESET_SEQ = "\033[0m"
-COLOR_SEQ = "\033[1;%dm"
-BOLD_SEQ = "\033[1m"
-
-
-def formatter_message(message, use_color=True):
-    if use_color:
-        message = message.replace("$RESET", RESET_SEQ).replace(
-            "$BOLD", BOLD_SEQ)
-    else:
-        message = message.replace("$RESET", "").replace("$BOLD", "")
-    return message
-
-COLORS = {
-    'WARNING': YELLOW,
-    'INFO': GREEN,
-    'DEBUG': BLUE,
-    'CRITICAL': YELLOW,
-    'ERROR': RED
-}
-
-
-class ColoredFormatter(logging.Formatter):
-    def __init__(self, msg, use_color=True):
-        logging.Formatter.__init__(self, msg)
-        self.use_color = use_color
-
-    def format(self, record):
-        levelname = record.levelname
-        if self.use_color and levelname in COLORS:
-            levelname_color = COLOR_SEQ % (
-                30 + COLORS[levelname]) + levelname + RESET_SEQ
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
-
-
-# Custom logger class with multiple destinations
-class ColoredLogger(logging.Logger):
-
-    FORMAT = "[$BOLD%(name)s$RESET][%(levelname)s] %(message)s"
-    COLOR_FORMAT = formatter_message(FORMAT, True)
-
-    def __init__(self, name):
-        logging.Logger.__init__(self, name, logging.DEBUG)
-
-        color_formatter = ColoredFormatter(self.COLOR_FORMAT)
-
-        console = logging.StreamHandler()
-        console.setFormatter(color_formatter)
-
-        self.addHandler(console)
-        return
-
-
-class LoggerTest(object):
-
-    def __init__(self):
-        self.prepare_logger()
-
-    def prepare_logger(self):
-        """
-        Create the logger for the script
-        @return True
-        """
-        logging.setLoggerClass(ColoredLogger)
-        self.logger = logging.getLogger('odoo_lint')
-        self.logger.setLevel(logging.DEBUG)
-        return True
-
-obj = LoggerTest()
-obj.logger.info('Hello world!')
-'''
